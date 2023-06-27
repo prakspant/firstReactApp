68a9b5958d0042c4062597590344c3326e32a39b
Prakhar Pant <prakharpant288@gmail.com>
2023-06-27 15:00:34 +0530

    passed all tests, needs beatification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: 'Loading...',
+      author: 'markus paolo'
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  getQuote() {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {this.setState({ quote: data.content, author: data.author});
+      console.log(data)})
+      .catch(error => console.error('Error:', error));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="quote-box">
+          <h1 id="text">{this.state.quote}</h1>
+          <p id="author">{this.state.author}</p>
+          <button id="new-quote" onClick={this.getQuote}>New Quote</button>
+          <br></br>
+          <a id="tweet-quote" href="https://twitter.com/intent/tweet">Post to twitter</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
